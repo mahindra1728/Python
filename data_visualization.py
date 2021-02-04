a09c7a7912387f6e2be583828262f6a6a0b1cf24
mahindra1728 <51349887+mahindra1728@users.noreply.github.com>
2021-02-04 20:05:24 +0530

    Add files via upload

--- data_visualization.py
+++ data_visualization.py
@@ -73,8 +73,106 @@ plt.xlim([0,200])
 # Represents the categorical data with rectangular bars with lengths proportional to the counts they represent
 #  Used to represent the frequency distribution of categorical data
 # A bar diagram makes it easy to compare sets of data between different groups 
+counts = pd.crosstab(data['borrower_genders'],columns = 'gender')
+plt.bar(data['borrower_genders'].unique(),[counts.iloc[0,0],counts.iloc[1,0],counts.iloc[2,0]])
 
-plt.bar(data['borrower_genders'])
+
+###############
+# Seaborn Library 
+###############
+
+import seaborn as sns
+
+sns.set(style= 'darkgrid')
+sns.regplot(x = data['lender_count'],y = data['loan_amount'])
+# Above is with regression fit line
+
+# Without regression fit line 
+
+sns.regplot(x = data['lender_count'], y = data['loan_amount'], fit_reg=(False),marker = 's')
+
+# Markers : * ,o,s
+
+# Scatter plot 
+sns.lmplot(x = 'lender_count', y ='loan_amount',data = data) # With regression
+
+sns.lmplot(x = 'lender_count', y ='loan_amount',data = data,fit_reg=(False),legend = True, hue = 'borrower_genders') # Without regression
+
+
+#############
+# Histogram 
+#############
+
+sns.distplot(data['loan_amount'],kde = False,bins = 1000)
+# sns.plt.xlim(0,15000)
+
+#############
+# Bar Plot
+#############
+
+sns.countplot(x = 'borrower_genders',data = data)
+
+##############
+# Grouped bar plot
+##############
+
+sns.countplot(x = 'borrower_genders',data = data, hue = 'currency_policy')
+
+
+##################
+# Box and whiskers plot : Numerical Variable
+##################
+
+sns.boxplot(y = data['lender_count'])
+
+
+##################
+# Box and whiskers plot : Numerical vs categorical variable
+##################
+
+sns.boxplot(x = data['borrower_genders'],y = data['lender_count'])
+
+
+##############
+# Grouped box and whiskers plot 
+##############
+
+sns.boxplot(x = data['borrower_genders'],y = data['lender_count'], hue = 'currency_policy', data = data)
+
+
+##################
+# Box whiskers plot and histogram
+##################
+
+f,(ax_box, ax_hist) = plt.subplots(2,gridspec_kw = {'height_ratios':(0.20,0.80)})
+
+sns.boxplot(data['rMPI'],ax = ax_box)
+
+sns.distplot(data['rMPI'], ax = ax_hist, kde = False)
+
+
+###################
+# Pairwise Plots
+##################
+
+# It is used to plot pairwise relationships in a dataset
+# Created scatterplots for joint relationships and histograms for univariate distributions
+
+sns.pairplot(data, kind = 'scatter', hue = 'currency_policy')
+plt.show
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
